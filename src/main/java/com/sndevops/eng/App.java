eeddec59294207a6cf86d36eb0fcd09cd866713e
sainadh777 <31940823+sainadh777@users.noreply.github.com>
2019-02-11 15:39:54 -0800

    commit :3.39

--- src/main/java/com/sndevops/eng/App.java
+++ src/main/java/com/sndevops/eng/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello devops 1246475753egttrshdgn" );
+        System.out.println( "Hello devops 989u" );
     }
 }
