a12a7b7b6c8d169cd1e8ad49b8cc22025a1ea16f
sainadh777 <31940823+sainadh777@users.noreply.github.com>
2019-02-11 09:24:24 -0800

    COMMIT : 98844

--- src/main/java/com/sndevops/eng/App.java
+++ src/main/java/com/sndevops/eng/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello devops 69469569562954" );
+        System.out.println( "Hello devops 45634" );
     }
 }
