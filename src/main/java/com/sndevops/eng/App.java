8ece50e324a869a06703553251f5da3c40b05c39
sainadh777 <31940823+sainadh777@users.noreply.github.com>
2019-02-25 19:01:04 -0800

    test:7 pm

--- src/main/java/com/sndevops/eng/App.java
+++ src/main/java/com/sndevops/eng/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello devops 9444444456677776788888" );
+        System.out.println( "Hello devops 9444" );
     }
 }
