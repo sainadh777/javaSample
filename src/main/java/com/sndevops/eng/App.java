2c261b7c787989625712a10433f7609fba572ea9
sainadh777 <31940823+sainadh777@users.noreply.github.com>
2019-03-01 17:35:38 -0800

    Update App.java

--- src/main/java/com/sndevops/eng/App.java
+++ src/main/java/com/sndevops/eng/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello devops 567678654567676" );
+        System.out.println( "Hello devops 567678654567676dgdth" );
     }
 }
