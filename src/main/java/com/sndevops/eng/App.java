84bca193794e36c392da3e41b95b8319eb156bf1
sainadh777 <31940823+sainadh777@users.noreply.github.com>
2019-03-15 15:11:59 -0700

    commit -874

--- src/main/java/com/sndevops/eng/App.java
+++ src/main/java/com/sndevops/eng/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Mr.Perfect 12367777" );
+        System.out.println( "Mr.Perfect 12367777rfgte" );
     }
 }
