2728e91d5b2f3fff35254a39c652125032b4a5db
sainadh777 <31940823+sainadh777@users.noreply.github.com>
2019-03-18 14:55:46 -0700

    commit -7849

--- src/main/java/com/sndevops/eng/App.java
+++ src/main/java/com/sndevops/eng/App.java
@@ -6,7 +6,7 @@ package com.sndevops.eng;
  */
 public class App
 {
-    public static void main( String[] args )
+    public static void main( String[] args ){
     
       System.out.println( "Mr.Perfect");
     }
