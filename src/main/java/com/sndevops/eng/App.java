26375e595ed6eda24a4004e39f36b1cf666e56c0
sainadh777 <31940823+sainadh777@users.noreply.github.com>
2019-03-18 13:28:43 -0700

    commit -r3

--- src/main/java/com/sndevops/eng/App.java
+++ src/main/java/com/sndevops/eng/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        wrsfSystem.out.println( "Mr.Perfect 7777rfgtedfrwbdfgdsfwffgfdgveatgdfvdeqafeqfsdsdsgdetdg" );
+        wrsfSystem.out.println( "Mr.Perfect 7777rfgtedfrwbdfgdsfwffgfdgveatgdfv" );
     }
 }
