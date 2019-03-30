5d65e8b013126359ed8788d3a6c25957e37e83da
sainadh777 <31940823+sainadh777@users.noreply.github.com>
2019-03-29 17:01:21 -0700

    commit -7939

--- src/main/java/com/sndevops/eng/App.java
+++ src/main/java/com/sndevops/eng/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args ){
     
-      System.out.println( "Mr.Perfe5brsfrwsfgvcryhfrhdryjgnb ");
+      System.out.println( "Mr. 234556 ");
     }
 }
