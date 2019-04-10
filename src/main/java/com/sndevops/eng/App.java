a64bdf20b9cf14740ec7f01c9553702cead2c41b
sainadh777 <31940823+sainadh777@users.noreply.github.com>
2019-04-10 16:11:54 -0700

    commit -43536

--- src/main/java/com/sndevops/eng/App.java
+++ src/main/java/com/sndevops/eng/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args ){
     
-      System.out.println( "Mr. 234556 chnage");
+      System.out.println( "Mr. 234556 chnage cwdfwd");
     }
 }
